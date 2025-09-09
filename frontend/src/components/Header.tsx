51dbf4102dbd18a0b8a133fe7ce2d46bbe9a8a42
Eduardo-Ennes <165242321+Eduardo-Ennes@users.noreply.github.com>
2025-09-08 21:29:04 -0300

    mudanças no header mais ainda não finalizadas

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,7 +14,7 @@ import {
 import { useState } from 'react';
 
 function Header() {
-  const [logado, setLogado] = useState(false)
+  const [logado, setLogado] = useState(true)
 
   return (
     <>
@@ -25,19 +25,41 @@ function Header() {
 
             <div className="flex w-full max-w-sm items-center gap-2">
               <Input type="email" placeholder="Email"/>
-              <Button type="button" variant="outline" className='bg-[oklch(14.5%_0_0)] cursor-pointer'>
+              <Button type="button" variant="outline" className='bg-[oklch(14.5%_0_0)] cursor-pointer text-base'>
                 Procurar
               </Button>
             </div>
 
-            {logado ? 
-              <Link to='#'>Encerrar sessão</Link>
+            {!logado ?
+              <NavigationMenu>
+                  <NavigationMenuList>
+                    <NavigationMenuItem>
+                      <NavigationMenuTrigger className="bg-[oklch(14.5%_0_0)] cursor-pointer text-base">
+                        Fazer login
+                      </NavigationMenuTrigger>
+                      <NavigationMenuContent className='!w-[13rem]'>
+                        <NavigationMenuLink asChild>
+                          <Link to="/login">Login</Link>
+                        </NavigationMenuLink>
+                        <NavigationMenuLink asChild>
+                          <Link to="/register">Cadastrar-se como usuário</Link>
+                        </NavigationMenuLink>
+                        <NavigationMenuLink asChild>
+                          <Link to="/affiliate-login">Login como afiliado</Link>
+                        </NavigationMenuLink>
+                        <NavigationMenuLink asChild>
+                          <Link to="/become-affiliate">Seja um afiliado</Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuContent>
+                    </NavigationMenuItem>
+                  </NavigationMenuList>
+                </NavigationMenu>
             :
               <NavigationMenu>
                   <NavigationMenuList>
                     <NavigationMenuItem>
-                      <NavigationMenuTrigger className="bg-[oklch(14.5%_0_0)] cursor-pointer">
-                        Fazer Login
+                      <NavigationMenuTrigger className="bg-[oklch(14.5%_0_0)] cursor-pointer text-base">
+                        My account
                       </NavigationMenuTrigger>
                       <NavigationMenuContent className='!w-[13rem]'>
                         <NavigationMenuLink asChild>
--- frontend/src/components/NavCategories.tsx
+++ frontend/src/components/NavCategories.tsx
@@ -1,25 +1,19 @@
 import { Link } from "react-router-dom";
-import MenuBurguer from '../assets/menu-burger.png'
+import IconApps from '../assets/apps.png'
 import '../css/navCategories.css'
 
 function NavCategories() {
   return (
     <>
         <nav className="bg-[oklch(14.5%_0_0)] flex flex-wrap p-2 items-center gap-[1rem] text-white">
-            <Link to='/' className="flex items-center gap-[0.25rem] border border-transparent px-2 py-1 hover:border-white hover:text-white transition-colors duration-300">
-                <img src={MenuBurguer} alt="Icone para buscar todos os cursos" /> 
+            <Link to='/' className="flex items-center gap-[0.30rem] border border-transparent px-2 py-1 hover:border-gray-600 hover:text-white transition-colors duration-300 ml-2">
+                <img src={IconApps} alt="Icone para buscar todos os cursos" /> 
                 <p>Todos</p>
             </Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">JavaScript</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">Python</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">Php</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">Machine Learning</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">Kali Linux</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">Sql</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">MySql</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">Docker</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">Terraform</Link>
-            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-white hover:text-white transition-colors duration-300">Aws Cloud</Link>
+            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-gray-600 hover:text-white transition-colors duration-300">JavaScript</Link>
+            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-gray-600 hover:text-white transition-colors duration-300">Python</Link>
+            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-gray-600 hover:text-white transition-colors duration-300">Php</Link>
+            <Link to='/categoria' className="border border-transparent px-[0.50rem] py-1 hover:border-gray-600 hover:text-white transition-colors duration-300">Machine Learning</Link>
         </nav>
     </>
   )
