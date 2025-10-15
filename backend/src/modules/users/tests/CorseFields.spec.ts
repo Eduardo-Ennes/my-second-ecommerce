45dba5d1751ee818791b25af88088f7a22f5cb43
Eduardo-Ennes <165242321+Eduardo-Ennes@users.noreply.github.com>
2025-10-14 22:41:16 -0300

    implementacao das validacoes e testes dos campos do curso

---
+++ backend/src/modules/users/tests/CorseFields.spec.ts
@@ -0,0 +1,122 @@
+import ValidationCorse from '../validations/validationFieldsCorse'
+
+const Course = {
+    name: 'Curso de python do básico ao avançado',
+    price: 139.99,
+    price_promotion: 29.99,
+    promotion: true,
+    technologies: [{name: 'python'}, {name: 'Django'}, {name: 'Mysql'}],
+    requisits: [{name: 'Ser um bosta'}, {name: 'Ser um merda'}],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Donec ac sapien at felis.'
+}
+
+
+describe('Name -> teste do nome do curso', () => {
+    it('Deve retornar: error de campo com mais de 75 caracteres', async () => {
+        const longName = 'A'.repeat(76)
+        const validation = await ValidationCorse.Fields({...Course, name: longName})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('O nome do curso pode conter apenas 75 caracteres.')
+    })
+
+    it('Deve retornar: error de campo vazio', async () => {
+        const validation = await ValidationCorse.Fields({...Course, name: ''})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('Nome do curso não pode ser vazio.')
+    })
+})
+
+
+describe('Price -> teste do preço do curso', () => {
+    it('Deve retornar: error de preço menor que R$10,00', async () => {
+        const validation = await ValidationCorse.Fields({...Course, price: 9.99})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('Preço mínimo do curso deve ser R$10,00 reais.')
+    })
+
+    it('Deve retornar: error de preço maior que R$200,00', async () => {
+        const validation = await ValidationCorse.Fields({...Course, price: 200.01})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('Preço máximo do curso deve ser R$200,00 reais.')
+    })
+})
+
+
+describe('Price Promotion -> teste do preço promocional do curso', () => {
+    it('Deve retornar: error de preço promocional menor que R$0,00', async () => {
+        const validation = await ValidationCorse.Fields({...Course, price_promotion: -1})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('Preço promocional não pode ser menor que R$0 reais')
+    })  
+
+    it('Deve retornar: error de preço promocional maior que o preço original', async () => {
+        const validation = await ValidationCorse.Fields({...Course, price_promotion: 140})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('Preço promocional não pode ser maior do que o valor original do curso. Preço promocional: R$140 | Preço original R$139.99.')
+    })
+})
+
+
+describe('Promotion -> teste do campo promoção do curso', () => {
+    it('Deve retornar: error de campo promoção diferente de boolean', async () => {
+        const validation = await ValidationCorse.Fields({...Course, promotion: 'true' as any})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('Houve um error ao declarar opção promocional!')
+    })
+})
+
+
+describe('Technologies -> teste do campo tecnologias do curso', () => {
+    it('Deve retornar: error de campo com mais de 75 caracteres', async () => {
+        const longTech = 'A'.repeat(76) 
+        const validation = await ValidationCorse.Fields({...Course, technologies: [{name: longTech}]})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe(`O campo de tecnologias do curso deve possuir no máximo 75 caracteres. ${longTech}.`)
+    })
+
+    it('Deve retornar: error de campo vazio', async () => {
+        const validation = await ValidationCorse.Fields({...Course, technologies: [{name: ''}]})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('O campo de tecnologias do curso não pode conter valores vazios.')
+    })
+})
+
+
+describe('Requisits -> teste do campo requisitos do curso', () => { 
+    it('Deve retornar: error de campo com mais de 75 caracteres', async () => {
+        const longRequisit = 'A'.repeat(76) 
+        const validation = await ValidationCorse.Fields({...Course, requisits: [{name: longRequisit}]})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe(`O campo de requisitos do curso deve possuir no máximo 75 caracteres. ${longRequisit}.`)
+    })
+
+    it('Deve retornar: error de campo vazio', async () => {
+        const validation = await ValidationCorse.Fields({...Course, requisits: [{name: ''}]})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('O campo de requisitos do curso não pode conter valores vazios.')
+    })
+})
+
+
+describe('Description -> teste do campo description do curso', () => {
+    it('Deve retornar: error de campo com mais de 3000 caracteres', async () => {
+        const longDescription = 'A'.repeat(3001) 
+        const validation = await ValidationCorse.Fields({...Course, description: longDescription})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('A descrição deve conter no máximo 3000 caracteres.')
+    })
+
+    it('Deve retornar: error de campo vazio', async () => {
+        const validation = await ValidationCorse.Fields({...Course, description: ''})
+        expect(validation.status).toBe(false)
+        expect(validation.error).toBe('A descrição não pode ser vazio.')
+    })
+})
+
+
+describe('All Fields -> teste de todos os campos do curso', () => {
+    it('Deve retornar: status true', async () => {
+        const validation = await ValidationCorse.Fields(Course)
+        expect(validation.status).toBe(true)
+    })  
+})
---
+++ backend/src/modules/users/validations/validationFieldsCorse.ts
@@ -0,0 +1,95 @@
+type Course = {
+  name: string
+  price: number
+  price_promotion?: number
+  promotion: boolean
+  technologies: { name: string }[]
+  requisits: { name: string }[]
+  description: string
+}
+
+
+class ValidationFieldsCorse {
+    async Fields (form: Course){
+        if('name' in form){
+            if(form.name.length > 75) {
+                return {status: false, error: 'O nome do curso pode conter apenas 75 caracteres.'}
+            }
+            else if(form.name.length < 1) {
+                return {status: false, error: 'Nome do curso não pode ser vazio.'}
+            }
+        }
+
+        if('price' in form){
+            if(form.price < 10){
+                return {status: false, error: 'Preço mínimo do curso deve ser R$10,00 reais.'}
+            }
+            else if(form.price > 200) {
+                return {status: false, error: 'Preço máximo do curso deve ser R$200,00 reais.'}
+            }
+        }
+
+        if('price_promotion' in form){
+            if(form.price_promotion < 0){
+                return {status: false, error: 'Preço promocional não pode ser menor que R$0 reais'}
+            }
+            else if(form.price_promotion > form.price){
+                return {status: false, error: `Preço promocional não pode ser maior do que o valor original do curso. Preço promocional: R$${form.price_promotion} | Preço original R$${form.price}.`}
+            }
+        }
+
+        if('promotion' in form){
+            if(typeof form.promotion !== 'boolean'){
+                return {status: false, error: 'Houve um error ao declarar opção promocional!'}
+            }
+        }
+
+        if('technologies' in form){
+            for (const tag of form.technologies) {
+                if (tag.name.length > 75) {
+                    return {
+                        status: false,
+                        error: `O campo de tecnologias do curso deve possuir no máximo 75 caracteres. ${tag.name}.`
+                    }
+                }
+                    if (tag.name.length < 1) {
+                    return {
+                        status: false,
+                        error: `O campo de tecnologias do curso não pode conter valores vazios.`
+                    }
+                }
+            }
+        }
+
+        if('requisits' in form){
+            for (const requisit of form.requisits) {
+                if (requisit.name.length > 75) {
+                    return {
+                        status: false,
+                        error: `O campo de requisitos do curso deve possuir no máximo 75 caracteres. ${requisit.name}.`
+                    }
+                }
+                if (requisit.name.length < 1) {
+                    return {
+                        status: false,
+                        error: `O campo de requisitos do curso não pode conter valores vazios.`
+                    }
+                }
+            }
+        }
+
+        if('description' in form){
+            if(form.description.length > 3000){
+                return {status: false, error: `A descrição deve conter no máximo 3000 caracteres.`}
+            }
+            else if(form.description.length < 1){
+                return {status: false, error: `A descrição não pode ser vazio.`}
+            }
+        }
+
+        return {status: true}
+    }
+}
+
+
+export default new ValidationFieldsCorse()
